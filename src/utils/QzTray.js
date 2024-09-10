a4a01a651426c33c2a2d9ec64ea21aaed6c99245
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-09-10 17:54:44 +0300

    printer

--- src/utils/QzTray.js
+++ src/utils/QzTray.js
@@ -81,7 +81,7 @@ class QzTrayPrinter {
                 console.log("Connected to QZ Tray");
             }
 
-            const printer = await this.findPrinter("zebra");
+            const printer = await this.findPrinter("ZDesigner");
             if (!printer) {
                 this.logError("No printers found.");
                 return;
